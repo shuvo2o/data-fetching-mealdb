ec5aab3e85fa367fa49cb45ed430580ced97b9f2
shuvo2o <eng.shuvo03@gmail.com>
2025-12-28 23:02:26 +0600

    styling the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Card from './components/Card';
 
 const App = () => {
   const [meals, setMeals] = useState([]);
@@ -20,12 +21,32 @@ const App = () => {
     })
   },[])
   return (
-    
-    <div className='min-h-screen bg-gray-700'>
-     <div>
-       <h2 className='text-xl text-center text-white'>Data Fetching from MealDB API </h2>
-     </div>
-    </div>
+    <section className='min-h-screen px-4 bg-gray-100'>
+         <div className='py-6 text-center text-white bg-teal-500'>
+           <h1 className='text-2xl font-bold'>Data Fetching from the Meal DB API</h1>
+         </div>
+   
+   
+         <div className='container py-8 mx-auto'>
+           {
+             loading && <p className='text-center text-blue-500 '>Loading...</p>
+           }
+   
+           {
+             error && <p className='text-center text-red-500'>Something went wrong!</p>
+           }
+   
+           <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+             {
+               meals.map((meal, index) => (
+                 <Card key={index} meal={meal}/>
+               ))
+             }
+           </div>
+         </div>
+   
+   
+       </section>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = ({meal}) => {
+  return (
+ <div className='overflow-hidden bg-white rounded-lg shadow-md'>
+        <img src={meal?.strMealThumb} alt="meal img" className='object-cover w-full h-48 transition-all duration-200 cursor-pointer md:h-56 hover:scale-105' />
+
+        <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>{meal.strMeal}</h2>
+            <p className='mt-2 text-sm text-gray-600'>{meal.strInstructions.slice(0, 100)}...</p>
+
+            <a href="#" className='block mt-4 text-blue-500 hover:underline'>Learn More</a>
+        </div>
+    </div>
+  )
+}
+
+export default Card
