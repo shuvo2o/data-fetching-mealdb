d8d59e8223caeadc0b21432c8a5640ff7491a9f9
shuvo2o <eng.shuvo03@gmail.com>
2025-12-28 22:45:32 +0600

    api itegrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  useEffect(()=>{
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+    .then((res) => res.json())
+    .then((data)=>{
+      setMeals(data.meals || []);
+      setLoading(false)
+    })
+    .catch(err =>{
+      console.log(err)
+      setError(err.message)
+      setLoading(false)
+    })
+  },[])
   return (
-    <div>
-      
+    
+    <div className='min-h-screen bg-gray-700'>
+     <div>
+       <h2 className='text-xl text-center text-white'>Data Fetching from MealDB API </h2>
+     </div>
     </div>
   )
 }
